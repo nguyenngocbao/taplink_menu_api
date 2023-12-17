45013c87bddb423f39535e7754a49c2aa5b5cdb3
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2023-12-17 14:22:59 +0700

    init project

---
+++ src/main/java/taplink/network/menu/api/TapLinkMenuApiApplication.java
@@ -0,0 +1,13 @@
+package taplink.network.menu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapLinkMenuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapLinkMenuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/taplink/network/menu/api/TaplinkMenuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package taplink.network.menu.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaplinkMenuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
