65c68625216b8307b37e072f30741e445c73b126
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2023-12-18 23:20:19 +0700

    MENU-16: [API][Store] Store API

--- src/main/java/taplink/network/menu/api/controller/GlobalExceptionController.java
+++ src/main/java/taplink/network/menu/api/controller/GlobalExceptionController.java
@@ -4,24 +4,57 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import taplink.network.menu.api.dto.ErrorDetails;
 import taplink.network.menu.api.exception.ResourceNotFoundException;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Date;
 
 @RestControllerAdvice
 public class GlobalExceptionController {
 
-    Logger logger = LoggerFactory.getLogger(GlobalExceptionController.class);
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionController.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest webRequest) {
         logger.error("ResourceNotFoundException", ex);
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<ErrorDetails> conflictData(Exception ex, WebRequest webRequest) {
+        logger.error("SQLIntegrityConstraintViolationException", ex);
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorDetails> methodNotSupportedException(Exception ex, WebRequest webRequest) {
+        logger.error("HttpRequestMethodNotSupportedException", ex);
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorDetails> badRequestHandler(Exception ex, WebRequest webRequest) {
+        logger.error("BadRequestException", ex);
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({Exception.class, IllegalStateException.class})
+    public ResponseEntity<ErrorDetails> handleUnhandledException(Exception ex, WebRequest webRequest) {
+        logger.error("UnhandledException", ex);
+        String message = "There was some error in processing your request. An exception has been logged";
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), message, webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
--- src/main/java/taplink/network/menu/api/dto/request/StoreRequestDto.java
+++ src/main/java/taplink/network/menu/api/dto/request/StoreRequestDto.java
@@ -1,4 +1,18 @@
 package taplink.network.menu.api.dto.request;
 
+import lombok.Data;
+
+@Data
 public class StoreRequestDto {
+    private String email;
+    private String name;
+    private String phone;
+    private boolean active;
+    private String wifiPass;
+    private Integer templateId;
+    private String address;
+    private Integer wardId;
+    private Integer districtId;
+    private Integer cityId;
+    private Integer storeTypeId;
 }
